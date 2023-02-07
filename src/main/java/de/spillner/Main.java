4e347a9addf35b33dd16352426d68cca7125a709
Lukas Spillner <spillnerlu+github@gmail.com>
2023-02-07 07:57:47 +0100

    Initial commit. IntelliJ IDEA "New Project" template with gradle (Groovy) and JDK 17 (Eclipse Temurin)

---
+++ src/main/java/de/spillner/Main.java
@@ -0,0 +1,11 @@
+package de.spillner;
+
+/**
+ *
+ * @author Lukas Spillner
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
