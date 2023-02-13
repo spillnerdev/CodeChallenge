c48451bfe63bc7e3dda50ac975a17faaedbc3198
Lukas Spillner <spillnerlu+github@gmail.com>
2023-02-13 20:39:45 +0100

    Fixed decimal point localisation error

--- src/main/java/de/spillner/sales/data/Receipt.java
+++ src/main/java/de/spillner/sales/data/Receipt.java
@@ -1,6 +1,7 @@
 package de.spillner.sales.data;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,8 @@ import java.util.stream.Collectors;
  */
 public record Receipt(Collection<InvoicePosition> positions, BigDecimal grossTotal, BigDecimal totalTax)
 {
+
+  private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#0.00");
   private static final String ORDER_TEMPLATE =
       """
           %s
@@ -18,14 +21,15 @@ public record Receipt(Collection<InvoicePosition> positions, BigDecimal grossTot
           Total: %.2f
           """;
 
-  private static final String POSITION_FORMAT = "%d %s: %.2f";
+  private static final String POSITION_FORMAT = "%d %s: %s";
 
   @Override
   public String toString()
   {
     String positions = positions().stream()
-        .map( ip -> POSITION_FORMAT.formatted( ip.amount().amount(), ip.name(), ip.total() ) )
+        .map( ip -> POSITION_FORMAT.formatted( ip.amount().amount(), ip.name(), DECIMAL_FORMAT.format( ip.total() ) ) )
         .collect( Collectors.joining( "\n" ) );
+
     return String.format( ORDER_TEMPLATE, positions, totalTax, grossTotal );
   }
 }
